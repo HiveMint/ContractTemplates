ae89162a40318b274597c110b7a428f58d88a9e9
y4000 <principal@y4000.xyz>
2022-08-31 16:30:18 -0500

    stubbed initial version of MerkleMint

---
+++ contracts/MerkleMint.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol"; // — uncomment when needed
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+
+/*
+ *  MerkleMint NFT Contract Template by HiveMint
+ */
+contract MerkleMint is ERC721, Pausable, Ownable, PaymentSplitter {
+    string public baseUri;
+    bytes32[] public merkleRoots;
+    uint256[] public merklePrices;
+    uint256[] public merkleSupply;
+    uint256[] public merkleStartTime;
+    uint256 public publicPrice;
+    uint256 public totalSupply;
+    uint256 public publicStartTime;
+
+    constructor(
+        string memory collectionName,
+        string memory tokenSymbol,
+        address[] memory _payees,
+        uint256[] memory _shares,
+        string memory _baseUri,
+        bytes32[] memory _merkleRoots,
+        uint256[] memory _merklePrices,
+        uint256[] memory _merkleSupply,
+        uint256[] memory _merkleStartTime,
+        uint256 memory _publicPrice,
+        uint256 memory _totalSupply,
+        uint256 memory _publicStartTime
+    ) ERC721(collectionName, tokenSymbol) PaymentSplitter(_payees, _shares) {
+        // set baseURI
+        baseURI = _baseUri;
+    }
+}
