e50871e827943e6bf6a1695911169cdb151451b6
y4000 <principal@y4000.xyz>
2022-09-05 12:16:03 -0500

    fleshing out mint functions

--- contracts/MerkleMint.sol
+++ contracts/MerkleMint.sol
@@ -13,7 +13,8 @@ import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
  *  MerkleMint NFT Contract Template by Y4000 for HiveMint
  */
 contract MerkleMint is ERC721, Pausable, Ownable, PaymentSplitter {
-    struct MerkleTier {
+
+    struct Tier {
         bytes32 root;
         uint256 price;
         uint256 supply;
@@ -21,40 +22,84 @@ contract MerkleMint is ERC721, Pausable, Ownable, PaymentSplitter {
         uint256 maxPerWallet;
     }
 
-    mapping(uint256 => mapping(address => uint256)) merkleMinted;
+    mapping(uint256 => mapping(address => uint256)) tierMinted;
+    mapping(address => uint256) publicMinted;
 
     string public baseUri;
-    MerkleTier[] public tiers;
-    uint256 public publicPrice;
-    uint256 public totalSupply;
-    uint256 public publicStartTime;
+    Tier[] public merkleTiers;
+    Tier public publicTier;
 
+    using Counters for Counters.Counter;
+    Counters.Counter private _mintCount;
+
+    /**
+        TODO: Licensing and Royalties
+     */
     constructor(
         string memory collectionName,
         string memory tokenSymbol,
         address[] memory _payees,
         uint256[] memory _shares,
         string memory _baseUri,
-        MerkleTier[] memory _tiers,
-        uint256 memory _publicPrice,
-        uint256 memory _totalSupply,
-        uint256 memory _publicStartTime
+        Tier[] memory _merkleTiers,
+        Tier memory _publicTier
     ) ERC721(collectionName, tokenSymbol) PaymentSplitter(_payees, _shares) {
         // initialize base variables
         baseURI = _baseUri;
-        tiers = _tiers;
-        publicPrice = _publicPrice;
-        totalSupply = _totalSupply;
-        publicStartTime = _publicStartTime;
+        merkleTiers = _merkleTiers;
+        publicTier = _publicTier;
+        // start counter at 1 so that the first NFT is #1
+        _mintCount.increment();
     }
 
     function airdrop(address[] calldata _recipients) public onlyOwner {
+        for (uint i=0; i<_recipients.length; i++) {
+            mint(_recipients[i], 1);
+        }
     }
 
-    function merkleMint(uint256 numTokens, bytes32[] calldata _merkleProof) public whenNotPaused {
+    function merkleMint(uint256 tierIdx, bytes32[] calldata _merkleProof, uint256 numTokens) public whenNotPaused {
+        require(
+            block.timestamp < merkleTiers[tierIdx].startTime,
+            "This tier mint has not yet started"
+        );
+        require(
+            merkleTiers[tierIdx].price * numTokens <= msg.value, 
+            "Insufficient funds for the requested transaction"
+        );
+        // TODO: check if max minted merkleTiers[tierIdx].maxPerWallet
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(_merkleProof, merkleTiers[tierIdx].root, leaf), 
+            "Not authorized for minting at this tier"
+        );
+        mint(msg.sender, numTokens);
+        // TODO: increment tier minted for this addy
     }
 
     function publicMint(uint256 numTokens) public payable whenNotPaused {
+        require(
+            block.timestamp < publicTier.startTime,
+            "Public Mint has not yet started"
+        );
+        require(
+            publicTier.price * numTokens <= msg.value, 
+            "Insufficient funds for the requested transaction"
+        );
+        // TODO: check if max minted publicTier.maxPerWallet
+        mint(msg.sender, numTokens);
+        // TODO: increment public minted for this addy
+    }
+
+    function mint(address addr, uint256 numTokens) private {
+        require(
+            _mintCount.current() + numTokens <= publicTier.supply, 
+            "Not enough supply remaining for the requested transaction"
+        );
+        for(uint256 i = 0; i < numTokens; i++) {
+            _safeMint(addr, _mintCount.current());
+            _mintCount.increment();
+        }
     }
 
 
