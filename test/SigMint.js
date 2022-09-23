3b566da7bee5bc85f711a40d5e8588b73bf735e3
y4000 <principal@y4000.xyz>
2022-09-23 13:58:09 -0500

    stubbed deploy in unit test

--- test/SigMint.js
+++ test/SigMint.js
@@ -3,3 +3,28 @@ const hardhat = require("hardhat");
 const { ethers } = hardhat;
 const { SigMinter } = require('../lib')
 
+describe("SigMint contract", function () {
+
+    let Token;
+    let hardhatToken;
+    let owner;
+    let hivemint;
+    let signer;
+    let generic_user;
+  
+    beforeEach(async () => {
+      Token = await ethers.getContractFactory("SigMint");
+      [owner, hivemint, signer, generic_user] = await ethers.getSigners();
+      hardhatToken = await Token.deploy(
+        "TestCollection",
+        "TSTCOL",
+        [owner.address, hivemint.address], 
+        [95,5], 
+        "https://hivemint.xyz/testtoken/", //TODO: staging base uri
+        [{}], // price, supply, startTime, maxPerWallet
+        {}, // publicTier
+        signer.address
+        );
+    });
+    
+});
