f032488161ded05f853e7334fd579de94786aaee
y4000 <principal@y4000.xyz>
2022-09-24 14:35:42 -0500

    deploy constructor fleshed out

--- test/SigMint.js
+++ test/SigMint.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const hardhat = require("hardhat");
 const { ethers } = hardhat;
 const { SigMinter } = require('../lib')
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("SigMint contract", function () {
 
@@ -15,14 +16,19 @@ describe("SigMint contract", function () {
     beforeEach(async () => {
       Token = await ethers.getContractFactory("SigMint");
       [owner, hivemint, signer, generic_user] = await ethers.getSigners();
+      let now = (await time.latest())
       hardhatToken = await Token.deploy(
         "TestCollection",
         "TSTCOL",
         [owner.address, hivemint.address], 
         [95,5], 
         "https://hivemint.xyz/testtoken/", //TODO: staging base uri
-        [{}], // price, supply, startTime, maxPerWallet
-        {}, // publicTier
+        [
+            {price: ethers.utils.parseEther('0.01'), supply: 100, startTime: now, maxPerWallet:5},
+            {price: ethers.utils.parseEther('0.02'), supply: 100, startTime: now, maxPerWallet:5},
+            {price: ethers.utils.parseEther('0.03'), supply: 100, startTime: now, maxPerWallet:5},
+        ], // tiers
+        {price: ethers.utils.parseEther('0.04'), supply: 100, startTime: now, maxPerWallet:5}, // publicTier
         signer.address
         );
     });
